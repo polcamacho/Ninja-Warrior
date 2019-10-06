1449c4ef53c01b6c1cc746aff93e03f722372640
polcamacho <47743832+polcamacho@users.noreply.github.com>
2019-10-06 18:41:27 +0200

    player and map assets

---
+++ Dev04_HandoutB/Game/maps/Assets/Forest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Forest" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="156" columns="12">
+ <image source="Assets.png" width="400" height="432"/>
+</tileset>
---
+++ Dev04_HandoutB/Game/maps/Assets/Player.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Player" tilewidth="14" tileheight="14" spacing="1" margin="1" tilecount="4897" columns="83">
+ <image source="../../Player/PSD/rogue.png" width="1258" height="886"/>
+</tileset>
