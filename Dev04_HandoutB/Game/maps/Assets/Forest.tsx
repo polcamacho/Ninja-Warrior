7fb13275bc7472e9ef2dc0c3ecd269bf7cb2ecad
polcamacho <47743832+polcamacho@users.noreply.github.com>
2019-10-07 19:33:46 +0200

    some tilesets

--- Dev04_HandoutB/Game/maps/Assets/Forest.tsx
+++ Dev04_HandoutB/Game/maps/Assets/Forest.tsx
@@ -1,4 +1,48 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="Forest" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="156" columns="12">
+<tileset version="1.2" tiledversion="1.2.4" name="Forest" tilewidth="14" tileheight="14" spacing="2" margin="2" tilecount="675" columns="25">
+ <grid orientation="orthogonal" width="32" height="32"/>
  <image source="Assets.png" width="400" height="432"/>
+ <terraintypes>
+  <terrain name="rock 1" tile="0"/>
+  <terrain name="high corridor 1" tile="0"/>
+  <terrain name="platform" tile="0"/>
+  <terrain name="cave" tile="0"/>
+  <terrain name="Nuevo Terreno" tile="0"/>
+  <terrain name="grass" tile="103"/>
+ </terraintypes>
+ <tile id="57" terrain="5,5,5,5"/>
+ <tile id="63" terrain=",1,,"/>
+ <tile id="64" terrain="1,1,,"/>
+ <tile id="65" terrain="1,,,"/>
+ <tile id="103" terrain="0,0,0,0"/>
+ <tile id="105" terrain=",,,1"/>
+ <tile id="106" terrain=",,1,1"/>
+ <tile id="107" terrain=",,1,"/>
+ <tile id="109" terrain=",,,5"/>
+ <tile id="110" terrain=",,5,5"/>
+ <tile id="111" terrain=",,5,"/>
+ <tile id="134" terrain=",5,,5">
+  <objectgroup draworder="index">
+   <object id="1" x="11" y="1"/>
+   <object id="2" x="8" y="2"/>
+   <object id="3" x="11" y="1" height="1"/>
+   <object id="4" x="-6" y="33" width="31" height="51"/>
+  </objectgroup>
+ </tile>
+ <tile id="135" terrain="3,3,3,3"/>
+ <tile id="139" terrain="1,1,1,1"/>
+ <tile id="140" terrain="5,,5,"/>
+ <tile id="152" terrain="0,0,0,0"/>
+ <tile id="163" terrain=",5,,"/>
+ <tile id="164" terrain="5,5,,"/>
+ <tile id="165" terrain="5,,,"/>
+ <tile id="208" terrain=",2,2,2"/>
+ <tile id="209" terrain="2,2,2,2"/>
+ <tile id="210" terrain="2,,2,2"/>
+ <tile id="258" terrain=",3,,"/>
+ <tile id="259" terrain="3,3,,"/>
+ <tile id="260" terrain="3,,,"/>
+ <tile id="287" terrain=",,,2"/>
+ <tile id="288" terrain=",,2,2"/>
+ <tile id="289" terrain=",,2,"/>
 </tileset>
