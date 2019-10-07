48013a368ee9f61502e4d4dc79083c8525fdfaf9
christianpi4 <47557433+christianpi4@users.noreply.github.com>
2019-10-07 08:10:21 +0200

    Try Map

--- Dev04_HandoutB/Game/maps/Assets/Forest.tsx
+++ Dev04_HandoutB/Game/maps/Assets/Forest.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="Forest" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="156" columns="12">
+<tileset version="1.2" tiledversion="1.2.4" name="Forest" tilewidth="14" tileheight="14" spacing="2" margin="2" tilecount="675" columns="25">
+ <grid orientation="orthogonal" width="32" height="32"/>
  <image source="Assets.png" width="400" height="432"/>
 </tileset>
