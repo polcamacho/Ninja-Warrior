072c7550a0ccbeb5a0c5a554b59a131cdea6efd4
christianpi4 <chricraft4100@gmail.com>
2019-10-07 22:43:13 +0200

    Tiled prueba

---
+++ Dev04_HandoutB/Game/maps/Assets/Backgroud3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Backgroud3" tilewidth="14" tileheight="14" spacing="1" margin="1" tilecount="350" columns="25">
+ <image source="Backgroud3.png" width="384" height="224"/>
+</tileset>
