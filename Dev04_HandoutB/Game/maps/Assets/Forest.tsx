b7b15c153ca2791274689c3957f6e1292d47b060
christianpi4 <chricraft4100@gmail.com>
2019-10-07 19:48:21 +0200

    Rock

--- Dev04_HandoutB/Game/maps/Assets/Forest.tsx
+++ Dev04_HandoutB/Game/maps/Assets/Forest.tsx
@@ -3,25 +3,29 @@
  <grid orientation="orthogonal" width="32" height="32"/>
  <image source="Assets.png" width="400" height="432"/>
  <terraintypes>
-  <terrain name="rock 1" tile="0"/>
   <terrain name="high corridor 1" tile="0"/>
   <terrain name="platform" tile="0"/>
   <terrain name="cave" tile="0"/>
   <terrain name="Nuevo Terreno" tile="0"/>
   <terrain name="grass" tile="103"/>
+  <terrain name="Rock" tile="0"/>
  </terraintypes>
+ <tile id="54" terrain=",5,,5"/>
+ <tile id="55" terrain=",,5,"/>
  <tile id="57" terrain="5,5,5,5"/>
- <tile id="63" terrain=",1,,"/>
- <tile id="64" terrain="1,1,,"/>
- <tile id="65" terrain="1,,,"/>
- <tile id="103" terrain="0,0,0,0"/>
- <tile id="105" terrain=",,,1"/>
- <tile id="106" terrain=",,1,1"/>
- <tile id="107" terrain=",,1,"/>
- <tile id="109" terrain=",,,5"/>
- <tile id="110" terrain=",,5,5"/>
- <tile id="111" terrain=",,5,"/>
- <tile id="134" terrain=",5,,5">
+ <tile id="63" terrain=",0,,"/>
+ <tile id="64" terrain="0,0,,"/>
+ <tile id="65" terrain="0,,,"/>
+ <tile id="101" terrain=",,,5"/>
+ <tile id="103" terrain="5,,,"/>
+ <tile id="105" terrain=",,,0"/>
+ <tile id="106" terrain=",,5,5"/>
+ <tile id="107" terrain=",,0,"/>
+ <tile id="109" terrain=",,,4"/>
+ <tile id="110" terrain=",,4,4"/>
+ <tile id="111" terrain=",,4,"/>
+ <tile id="130" terrain=",5,,5"/>
+ <tile id="134" terrain=",4,,4">
   <objectgroup draworder="index">
    <object id="1" x="11" y="1"/>
    <object id="2" x="8" y="2"/>
@@ -29,20 +33,20 @@
    <object id="4" x="-6" y="33" width="31" height="51"/>
   </objectgroup>
  </tile>
- <tile id="135" terrain="3,3,3,3"/>
- <tile id="139" terrain="1,1,1,1"/>
- <tile id="140" terrain="5,,5,"/>
- <tile id="152" terrain="0,0,0,0"/>
- <tile id="163" terrain=",5,,"/>
+ <tile id="135" terrain="2,2,2,2"/>
+ <tile id="139" terrain="0,0,0,0"/>
+ <tile id="140" terrain="4,,4,"/>
+ <tile id="152" terrain=",5,,"/>
+ <tile id="163" terrain=",4,,"/>
  <tile id="164" terrain="5,5,,"/>
- <tile id="165" terrain="5,,,"/>
- <tile id="208" terrain=",2,2,2"/>
- <tile id="209" terrain="2,2,2,2"/>
- <tile id="210" terrain="2,,2,2"/>
- <tile id="258" terrain=",3,,"/>
- <tile id="259" terrain="3,3,,"/>
- <tile id="260" terrain="3,,,"/>
- <tile id="287" terrain=",,,2"/>
- <tile id="288" terrain=",,2,2"/>
- <tile id="289" terrain=",,2,"/>
+ <tile id="165" terrain="4,,,"/>
+ <tile id="208" terrain=",1,1,1"/>
+ <tile id="209" terrain="1,1,1,1"/>
+ <tile id="210" terrain="1,,1,1"/>
+ <tile id="258" terrain=",2,,"/>
+ <tile id="259" terrain="2,2,,"/>
+ <tile id="260" terrain="2,,,"/>
+ <tile id="287" terrain=",,,1"/>
+ <tile id="288" terrain=",,1,1"/>
+ <tile id="289" terrain=",,1,"/>
 </tileset>
