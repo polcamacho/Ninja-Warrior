53b10ce29d3d2b382e5a9d92a0514ff4cd0d26a4
christianpi4 <chricraft4100@gmail.com>
2019-10-11 13:43:52 +0200

    Player.cpp and Player.h called all functions

--- Dev04_HandoutB/Motor2D/j1Player.h
+++ Dev04_HandoutB/Motor2D/j1Player.h
@@ -6,7 +6,7 @@
 #include "p2List.h"
 #include "p2Point.h"
 #include "j1Module.h"
-#include "p2Animayio
+#include "p2Animation.h"
 
 
 // ----------------------------------------------------
@@ -14,57 +14,84 @@ class j1Player : public j1Module
 {
 public:
 
+	//Constructor
 	j1Player();
 
 	// Destructor
 	virtual ~j1Player();
 
-	// Called before render is available
-	bool Awake(pugi::xml_node& conf);
+	//Called at first
+	bool Start();
+	bool PreUpdate();
+	bool Update();
+	bool PostUpdate();
 
+	// Called before render is available
+	
+	//bool Awake(pugi::xml_node& conf);
+
+	
 	// Called each loop iteration
-	void Draw();
+	
+	void DrawPlayer();
+
+	bool Load(pugi::xml_node&);
+	bool Save(pugi::xml_node&) const;
+
 
 	// Called before quitting
 	bool CleanUp();
 
-	// Load new map
-	bool Load(const char* path);
-
-
-
-	// TODO 8: Create a method that translates x,y coordinates from map positions to world positions
-	iPoint GetWorldPos(int x, int y) const; //Gets the position in the world
+	//Called to start the player at initial pos
+	void Restart();
+	
+	//Change the sound depending the animation
+	void SFX(int channel, int repeat);
+	
+	//Puts the camera on player and follows
+	bool Camera_Player();
 
 private:
 
-	bool LoadPlayer(pugi::xml_node& node); //Load the map
-
+	
 public:
 
 
 private:
 
-	pugi::xml_document	map_file;
+	PlayerData	data_player;
 	p2SString			folder;
-	bool				map_loaded;
-	uint idtile = 0;
-	SDL_Texture* texture;
+	p2SString			texture;
 
+};
+
+struct PlayerData
+{
+
+	Animation*		current_animation;
 	Animation		idle;
+	Animation		walk;
 	Animation		running;
-	Animation		jumping_up;
-	Animation		falling;
-	Animation		dashing;
+	Animation		jump;
+	Animation		fall;
 	Animation		dying;
-	j1Timer			dashtimer;
+
 	p2SString		jumpFX;
 	p2SString		deathFX;
-	p2SString		landFX;
-	p2SString		dashFX;
-	p2SString		pickupFX;
-	p2SString		timestopFX;
+	
+	iPoint position;	//Position of the player (x,y)
+	iPoint a;	//Acceleration (x,y)
+	iPoint vj;	//Velocity of the jump (x,y)
+	iPoint v;	//Velocity of the player (x,y)
 
+	SDL_Rect		col;
+	SDL_Texture*	Tex_Player;
+
+	float jump_time;	//Time of the jump
+	
+	bool injump;	//Says that the player is jumping or not
+	
+	void Pushbacks();
 
 };
 
