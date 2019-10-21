908e3d2e2b5bcbf04c5f9c0ad11cdea2eae47451
polcamacho <47743832+polcamacho@users.noreply.github.com>
2019-10-21 19:11:36 +0200

    Some changes
    new states added, pivot added and many tries to solve run and jump

--- Dev04_HandoutB/Motor2D/p2Animation.h
+++ Dev04_HandoutB/Motor2D/p2Animation.h
@@ -9,28 +9,31 @@ class Animation
 {
 public:
 	bool loop = true;
-	float speed = 1.0f;
 	SDL_Rect frames[MAX_FRAMES];
-
+	int pivotx[MAX_FRAMES];
+	int pivoty[MAX_FRAMES];
+	double speed[MAX_FRAMES];
+	int last_frame = 0;
 
 private:
 
 	float current_frame;
-	int last_frame = 0;
+	int speeddelay = 0;
 	int loops = 0;
 
 public:
 
-	void PushBack(const SDL_Rect& rect)
+	void PushBack(const SDL_Rect& rect, double delay, int px, int py)
 	{
-		if (last_frame < MAX_FRAMES)
-			frames[last_frame++] = rect;
-		assert(last_frame < MAX_FRAMES);
+		pivotx[last_frame] = px;
+		pivoty[last_frame] = py;
+		frames[last_frame++] = rect;
+		speed[speeddelay++] = delay;
 	}
 
 	SDL_Rect& GetCurrentFrame()
 	{
-		current_frame += speed;
+		current_frame += speed[(int)current_frame];
 		
 
 		if (current_frame >= last_frame)
