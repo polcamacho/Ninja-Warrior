17a5e679168cb52b6296104408866f204807842d
christianpi4 <chricraft4100@gmail.com>
2019-11-30 22:15:07 +0100

    Removed Rubish

--- Dev04_HandoutB/Game/maps/Assets/Cossas/Backgroud2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="Backgroud2" tilewidth="14" tileheight="14" spacing="1" margin="1" tilecount="350" columns="25">
- <image source="Backgroud2.png" width="384" height="224"/>
-</tileset>
--- Dev04_HandoutB/Game/maps/Assets/Cossas/Forest2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="Forest2" tilewidth="14" tileheight="14" spacing="1" margin="1" tilecount="820" columns="41">
- <image source="../Jungle Asset Pack/jungle tileset/jungle tileset.png" width="624" height="304"/>
-</tileset>
