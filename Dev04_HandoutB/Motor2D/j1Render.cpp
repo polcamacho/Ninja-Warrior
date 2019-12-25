e21a81811adfdc84f8a6edfe2340b8ad7e6b5486
christianpi4 <chricraft4100@gmail.com>
2019-12-25 18:31:48 +0100

    Cap and Pause

--- Dev04_HandoutB/Motor2D/j1Render.cpp
+++ Dev04_HandoutB/Motor2D/j1Render.cpp
@@ -10,7 +10,7 @@
 #include "j1Scene.h"
 #include "..//Brofiler/Brofiler.h"
 
-#define VSYNC true
+#define VSYNC false
 
 j1Render::j1Render() : j1Module()
 {
@@ -37,6 +37,7 @@ bool j1Render::Awake(pugi::xml_node& config)
 	{
 		flags |= SDL_RENDERER_PRESENTVSYNC;
 		LOG("Using vsync");
+		App->vsync = true;
 	}
 
 	renderer = SDL_CreateRenderer(App->win->window, -1, flags);
