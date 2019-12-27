e4ec2e8bc3b7e091c06233a68bb43bbe73ce58dc
polcamacho <47743832+polcamacho@users.noreply.github.com>
2019-12-27 17:31:07 +0100

    deleted button slider volume code
    It doesnt work :(

--- Dev04_HandoutB/Motor2D/UI_Slider.cpp
+++ Dev04_HandoutB/Motor2D/UI_Slider.cpp
@@ -5,7 +5,7 @@
 #include "UI_Slider.h"
 #include "j1Render.h"
 
-UI_Slider::UI_Slider(int x, int y, UI_Type type, SDL_Rect scrollbar, SDL_Rect button, SDL_Rect left_button, float width, UI_element* parent, j1Module* Observer) : UI_element(x, y, type, parent, Observer)
+UI_Slider::UI_Slider(int x, int y, UI_Type type, SDL_Rect scrollbar, SDL_Rect button, float width, UI_element* parent, j1Module* Observer) : UI_element(x, y, type, parent, Observer)
 {
 	texture = App->gui->GetAtlas();
 	
@@ -15,14 +15,12 @@ UI_Slider::UI_Slider(int x, int y, UI_Type type, SDL_Rect scrollbar, SDL_Rect bu
 	current_point = x + 89;
 
 	Button_Scrollbar = button;
-	L_Button = left_button;
 
 	mouse_position_in_button = -1;
 	dimensions.w = button.w + 150;
 	dimensions.h = button.h + 150;
 
 	App->audio->Change_Volume_Music(get_value());
-
 
 }
 
@@ -32,21 +30,24 @@ UI_Slider::~UI_Slider() {
 bool UI_Slider::Update(float dt)
 {
 
-	if (IsIntersection() == true && App->input->GetMouseButtonDown(SDL_BUTTON_LEFT)==KEY_REPEAT) {
+	if (IsIntersection() == true) {
+		if (App->input->GetMouseButtonDown(SDL_BUTTON_LEFT) == KEY_REPEAT) {
 
-		if (mouse_position_in_button == -1) {
-			App->input->GetMousePosition(new_mouse_pos.x, new_mouse_pos.y);
-			mouse_position_in_button = new_mouse_pos.x;
-			
+
+			if (mouse_position_in_button == -1) {
+				App->input->GetMousePosition(new_mouse_pos.x, new_mouse_pos.y);
+				mouse_position_in_button = new_mouse_pos.x;
+
+			}
+
+			Mouse_Is_Moving();
+			App->audio->Change_Volume_Music(get_value());
 		}
-
-		Mouse_Is_Moving();
-		App->audio->Change_Volume_Music(get_value());
 	}
 	else {
 
 		mouse_position_in_button = -1;
-		Slider_volume_buttons();
+		
 	}
 	
 	
@@ -92,7 +93,6 @@ bool UI_Slider::Draw() {
 
 	App->render->Blit(texture, pos.x, pos.y, &Scrollbar, SDL_FLIP_NONE, 1.0f);
 	App->render->Blit(texture, current_point, pos.y-4, &Button_Scrollbar, SDL_FLIP_NONE, 1.0f);
-	App->render->Blit(texture, pos.x-30, pos.y - 4, &L_Button, SDL_FLIP_NONE, 1.0f);
 
 	return true;
 
@@ -113,16 +113,3 @@ float UI_Slider::get_value()
 	return ret;
 
 }
-
-void UI_Slider::Slider_volume_buttons()
-{
-
-	UI_Button* left = (UI_Button*)(pos.x, pos.y, Button_slider_music_left, L_Button, L_Button, L_Button, NULL, this);
-
-	if (left->t==Button_slider_music_left && IsIntersection() == true && App->input->GetMouseButtonDown(SDL_BUTTON_LEFT) == KEY_DOWN) {
-		LOG("%s", left->t);
-		App->audio->Change_Volume(0.5, 0);
-	}
-
-}
-
--- Dev04_HandoutB/Motor2D/UI_Slider.h
+++ Dev04_HandoutB/Motor2D/UI_Slider.h
@@ -14,7 +14,7 @@ class UI_Slider : public UI_element
 public:
 
 
-	UI_Slider(int x, int y, UI_Type type, SDL_Rect scrollbar, SDL_Rect button, SDL_Rect left_button, float width, UI_element* parent, j1Module* Observer);
+	UI_Slider(int x, int y, UI_Type type, SDL_Rect scrollbar, SDL_Rect button, float width, UI_element* parent, j1Module* Observer);
 
 	~UI_Slider();
 
@@ -25,8 +25,6 @@ public:
 	bool Mouse_Is_Moving();
 
 	float get_value();
-
-	void Slider_volume_buttons();
 
 public:
 
@@ -40,8 +38,6 @@ private:
 	
 	SDL_Rect Button_Scrollbar;
 	SDL_Rect Scrollbar;
-	SDL_Rect L_Button;
-	SDL_Rect R_Button;
 
 	float initial_point, max_point, current_point;
 };
